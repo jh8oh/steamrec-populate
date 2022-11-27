390402d90f0717685fbe4f4433db7d7a26a399fb
jh8oh <ohjiho.ca@gmail.com>
2022-11-27 09:12:12 +0700

    Merge load and helper

--- src/functions/helper.js
+++ src/functions/helper.js
@@ -1,3 +1,35 @@
+import fs from "fs";
+
+export async function loadFromFile(path, isJson) {
+  if (!(await checkFileExists(path))) {
+    return Promise.reject(new Error(`${path} does not exist`));
+  }
+
+  return readFile(path).then((it) => {
+    if (isJson) {
+      return JSON.parse(it);
+    } else {
+      return it;
+    }
+  });
+}
+
+async function checkFileExists(file) {
+  return await fs.promises
+    .access(file, fs.constants.F_OK)
+    .then(() => true)
+    .catch(() => false);
+}
+
+function readFile(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) return reject(err);
+      return resolve(data);
+    });
+  });
+}
+
 export function removeDuplicate(array) {
   var check = new Set();
   return array.filter((it) => !check.has(it) && check.add(it));
