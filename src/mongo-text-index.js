30c765d82303c4015ac02cad8ab8d0042d3108d2
jh8oh <ohjiho.ca@gmail.com>
2022-12-13 19:35:22 +0700

    Create mongo db text index

---
+++ src/mongo-text-index.js
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
+MongoClient.connect(process.env.MONGODB_URI)
+  .then((client) => {
+    client
+      .db("steamrec")
+      .collection("games")
+      .createIndex(
+        {
+          developers: "text",
+          publishers: "text",
+          categories: "text",
+          genres: "text",
+          "tags.description": "text",
+        },
+        {
+          name: "RecommendIndex",
+          weights: {
+            developers: 3,
+            publishers: 3,
+            categories: 1,
+            genres: 2,
+            "tags.description": 4,
+          },
+        }
+      );
+  })
+  .catch((err) => {
+    throw err;
+  });
